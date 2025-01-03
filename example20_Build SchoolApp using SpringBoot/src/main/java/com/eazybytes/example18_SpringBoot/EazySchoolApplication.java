1481c06c48ef4eacd5d3b8c7b25ef9a0ee18ef09
Tahir Mehboob <122098506+Tahir-Mehboob@users.noreply.github.com>
2025-01-03 18:19:09 +0500

    HomePage Ui Completed and Tested
    Adding Folder in Resources/static
    1)Assets
    2)Css
    3)images
    4)js
    5)webfonts
    
    Adding 600 lines code in home.html for display HomePage

---
+++ example20_Build SchoolApp using SpringBoot/src/main/java/com/eazybytes/example18_SpringBoot/EazySchoolApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.example18_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+// create exclusion list when we didn't create bean and configure by spring boot using autoconfigure
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class EazySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazySchoolApplication.class, args);
+	}
+}
---
+++ example20_Build SchoolApp using SpringBoot/src/main/java/com/eazybytes/example18_SpringBoot/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.eazybytes.example18_SpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+   // @RequestMapping("/home")
+    @RequestMapping(value={"","/","home"})
+    public String displayHomePage() {
+        return "home.html";
+    }
+}
