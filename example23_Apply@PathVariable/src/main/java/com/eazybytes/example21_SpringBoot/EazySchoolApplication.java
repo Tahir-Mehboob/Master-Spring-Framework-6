920afb613b32676e1336a9ce657d532dbeee5717
Tahir Mehboob <122098506+Tahir-Mehboob@users.noreply.github.com>
2025-01-15 21:09:24 +0500

    Adding Path Param by using @PathVariable
    1) adding all in footer.html in th:href = @{/holidays/all}
    2) adding display param in getMapping anno
    3) @pathVariable in mehtod param and String dispaly
    4) apply if else is in display all festival and federal

---
+++ example23_Apply@PathVariable/src/main/java/com/eazybytes/example21_SpringBoot/EazySchoolApplication.java
@@ -0,0 +1,14 @@
+package com.eazybytes.example21_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// create exclusion list when we didn't create bean and configure by spring boot using autoconfigure
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class EazySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazySchoolApplication.class, args);
+	}
+}
